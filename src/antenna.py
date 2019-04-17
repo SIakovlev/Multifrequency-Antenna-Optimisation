e93016e062d0a4645a61c4cf41c70534aaaae440
Sergey Iakovlev <siakovlev@student.unimelb.edu.au>
2019-04-18 02:29:57 +1000

    SI: add objective_function.py

--- src/antenna.py
+++ src/antenna.py
@@ -4,64 +4,7 @@ import scipy
 import matplotlib.colors as colors
 from scipy.optimize import minimize, LinearConstraint, BFGS, check_grad
 from utils import plotter
-
-
-def fg(A, x, b, weight):
-    result = 0
-    grad_list = []
-    for A_i, x_i, b_i, w_i in zip(A, x, b, weight):
-
-        result += w_i * np.linalg.norm(abs(A_i @ x_i) ** 2 - abs(b_i) ** 2, 2)**2
-
-        grad_k = 0
-        for k in range(A_i.shape[0]):
-            Q_k = np.real(A_i[k, :].reshape(1, -1).conj().T @ A_i[k, :].reshape(1, -1))
-            grad_k += 4 * (x_i.T @ Q_k @ x_i - abs(b_i[k, :]) ** 2) * Q_k @ x_i * x_i
-        grad_list.append(w_i * grad_k)
-
-    return result, np.vstack(grad_list) + np.random.randn()
-
-
-def g(A, x, b, weight):
-
-    # TODO: double check, the error between this and the numerical approximation is about 0.005...
-    # not used at the moment
-    grad_list = []
-    for A_i, x_i, b_i, w_i in zip(A, x, b, weight):
-
-        grad_k = 0
-        for k in range(A_i.shape[0]):
-            Q_k = np.real(A_i[k, :].reshape(1, -1).conj().T @ A_i[k, :].reshape(1, -1))
-            grad_k += 4 * (x_i.T @ Q_k @ x_i - abs(b_i[k, :]) ** 2) * Q_k @ x_i * x_i
-        grad_list.append(w_i * grad_k)
-
-    return np.vstack(grad_list).reshape(-1, )
-
-
-def f(A, x, b, weight):
-
-    result = 0
-    for A_i, x_i, b_i, w_i in zip(A, x, b, weight):
-
-        result += w_i * np.linalg.norm(abs(A_i @ x_i) ** 2 - abs(b_i) ** 2, 2)**2
-
-    return result
-
-
-def h(A, x, b, weight):
-    hess_list = []
-    for A_i, x_i, b_i, w_i in zip(A, x, b, weight):
-
-        hess_k = 0
-        for k in range(A_i.shape[0]):
-            Q_k = np.real(A_i[k, :].reshape(1, -1).conj().T @ A_i[k, :].reshape(1, -1))
-            t0 = Q_k @ x_i * x_i
-            t1 = x_i.T @ Q_k @ x_i - abs(b_i[k, :]) ** 2
-            hess_k += 8 * t0 @ ((x_i.T @ Q_k) * x_i.T)\
-                      + 4 * t1 * np.diag(x_i.squeeze()) @ Q_k @ np.diag(x_i.squeeze()) + 4 * t1 * np.diag(t0.squeeze())
-        hess_list.append(hess_k)
-
-    return scipy.linalg.block_diag(*hess_list)
+from objective_function import f, g, h
 
 
 class Antenna:
@@ -116,30 +59,24 @@ class Antenna:
                 style_mod=[colors_list[i] for i in range(len(self.afs))])
 
     def set_hessian(self, weights):
-
         def hessian(J):
             J = J.reshape(-1, 1)
             list_J = np.split(np.exp(J), self.n_currents)
             return h(self.afs, list_J, self.beams, weights)
-
         self.hess = hessian
 
     def set_jacobian(self, weights):
-
         def jacobian(J):
             J = J.reshape(-1, 1)
             list_J = np.split(np.exp(J), self.n_currents)
             return g(self.afs, list_J, self.beams, weights)
-
         self.jac = jacobian
 
     def set_objective(self, weights):
-
         def objective(J):
             J = J.reshape(-1, 1)
             list_J = np.split(np.exp(J), self.n_currents)
             return f(self.afs, list_J, self.beams, weights)
-
         self.objective = objective
 
     def set_callback(self):
---
+++ src/objective_function.py
@@ -0,0 +1,55 @@
+import numpy as np
+import scipy
+
+
+def fg(A, x, b, weight):
+    result = 0
+    grad_list = []
+    for A_i, x_i, b_i, w_i in zip(A, x, b, weight):
+
+        result += w_i * np.linalg.norm(abs(A_i @ x_i) ** 2 - abs(b_i) ** 2, 2) ** 2
+
+        grad_k = 0
+        for k in range(A_i.shape[0]):
+            Q_k = np.real(A_i[k, :].reshape(1, -1).conj().T @ A_i[k, :].reshape(1, -1))
+            grad_k += 4 * (x_i.T @ Q_k @ x_i - abs(b_i[k, :]) ** 2) * Q_k @ x_i * x_i
+        grad_list.append(w_i * grad_k)
+
+    return result, np.vstack(grad_list) + np.random.randn()
+
+
+def g(A, x, b, weight):
+    grad_list = []
+    for A_i, x_i, b_i, w_i in zip(A, x, b, weight):
+
+        grad_k = 0
+        for k in range(A_i.shape[0]):
+            Q_k = np.real(A_i[k, :].reshape(1, -1).conj().T @ A_i[k, :].reshape(1, -1))
+            grad_k += 4 * (x_i.T @ Q_k @ x_i - abs(b_i[k, :]) ** 2) * Q_k @ x_i * x_i
+        grad_list.append(w_i * grad_k)
+
+    return np.vstack(grad_list).reshape(-1, )
+
+
+def f(A, x, b, weight):
+    result = 0
+    for A_i, x_i, b_i, w_i in zip(A, x, b, weight):
+        result += w_i * np.linalg.norm(abs(A_i @ x_i) ** 2 - abs(b_i) ** 2, 2) ** 2
+
+    return result
+
+
+def h(A, x, b, weight):
+    hess_list = []
+    for A_i, x_i, b_i, w_i in zip(A, x, b, weight):
+
+        hess_k = 0
+        for k in range(A_i.shape[0]):
+            Q_k = np.real(A_i[k, :].reshape(1, -1).conj().T @ A_i[k, :].reshape(1, -1))
+            t0 = Q_k @ x_i * x_i
+            t1 = x_i.T @ Q_k @ x_i - abs(b_i[k, :]) ** 2
+            hess_k += 8 * t0 @ ((x_i.T @ Q_k) * x_i.T) \
+                      + 4 * t1 * np.diag(x_i.squeeze()) @ Q_k @ np.diag(x_i.squeeze()) + 4 * t1 * np.diag(t0.squeeze())
+        hess_list.append(hess_k)
+
+    return scipy.linalg.block_diag(*hess_list)
