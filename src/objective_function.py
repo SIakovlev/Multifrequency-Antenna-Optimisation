4cdbf62e1bba2a70b4600268788ab8cfeb4c8c13
Sergey Iakovlev <siakovlev@student.unimelb.edu.au>
2019-04-24 16:24:50 +1000

    SI: add optional offset to grad, f and hessian

--- src/objective_function.py
+++ src/objective_function.py
@@ -18,28 +18,28 @@ def fg(A, x, b, weight):
     return result, np.vstack(grad_list) + np.random.randn()
 
 
-def g(A, x, b, weight):
+def g(A, x, b, weight, offset=0):
     grad_list = []
     for A_i, x_i, b_i, w_i in zip(A, x, b, weight):
 
         grad_k = 0
         for k in range(A_i.shape[0]):
             Q_k = np.real(A_i[k, :].reshape(1, -1).conj().T @ A_i[k, :].reshape(1, -1))
-            grad_k += 4 * (x_i.T @ Q_k @ x_i - abs(b_i[k, :]) ** 2) * Q_k @ x_i * x_i
+            grad_k += 4 * (x_i.T @ Q_k @ x_i - abs(b_i[k, :]) ** 2 - offset) * Q_k @ x_i * x_i
         grad_list.append(w_i * grad_k)
 
     return np.vstack(grad_list).reshape(-1, )
 
 
-def f(A, x, b, weight):
+def f(A, x, b, weight, offset=0):
     result = 0
     for A_i, x_i, b_i, w_i in zip(A, x, b, weight):
-        result += w_i * np.linalg.norm(abs(A_i @ x_i) ** 2 - abs(b_i) ** 2, 2) ** 2
+        result += w_i * np.linalg.norm(abs(A_i @ x_i) ** 2 - abs(b_i) ** 2 - offset, 2) ** 2
 
     return result
 
 
-def h(A, x, b, weight):
+def h(A, x, b, weight, offset=0):
     hess_list = []
     for A_i, x_i, b_i, w_i in zip(A, x, b, weight):
 
@@ -47,7 +47,7 @@ def h(A, x, b, weight):
         for k in range(A_i.shape[0]):
             Q_k = np.real(A_i[k, :].reshape(1, -1).conj().T @ A_i[k, :].reshape(1, -1))
             t0 = Q_k @ x_i * x_i
-            t1 = x_i.T @ Q_k @ x_i - abs(b_i[k, :]) ** 2
+            t1 = x_i.T @ Q_k @ x_i - abs(b_i[k, :]) ** 2 - offset
             hess_k += 8 * t0 @ ((x_i.T @ Q_k) * x_i.T) \
                       + 4 * t1 * np.diag(x_i.squeeze()) @ Q_k @ np.diag(x_i.squeeze()) + 4 * t1 * np.diag(t0.squeeze())
         hess_list.append(hess_k)
