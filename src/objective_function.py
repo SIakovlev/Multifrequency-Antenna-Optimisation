b2f0974e2ffe588875afd5ce4fe98bdde1513eb3
Sergey Iakovlev <siakovlev@student.unimelb.edu.au>
2019-05-04 18:08:03 +1000

    SI: add mask support to both h and g functions

--- src/objective_function.py
+++ src/objective_function.py
@@ -2,7 +2,7 @@ import numpy as np
 import scipy
 
 
-def g(A, x, b, weight, offset=0):
+def g(A, x, b, weight, offset=0, mask=None):
     grad_list = []
     for A_i, x_i, b_i, w_i in zip(A, x, b, weight):
 
@@ -12,21 +12,23 @@ def g(A, x, b, weight, offset=0):
             grad_k += 4 * (x_i.T @ Q_k @ x_i - abs(b_i[k, :]) ** 2 - offset) * Q_k @ x_i * x_i
         grad_list.append(w_i * grad_k)
 
-    result = np.vstack(grad_list).reshape(-1, )
-
-    # TODO: add permutation based on mask of currents
-    return result
+    if mask is None:
+        result = np.vstack(grad_list).reshape(-1, )
+        return result
+    else:
+        grads = np.array(grad_list).reshape(len(x), -1)
+        grads = np.sum(grads * mask, axis=0)
+        return grads
 
 
 def f(A, x, b, weight, offset=0):
     result = 0
     for A_i, x_i, b_i, w_i in zip(A, x, b, weight):
         result += w_i * np.linalg.norm(abs(A_i @ x_i) ** 2 - abs(b_i) ** 2 - offset, 2) ** 2
-
     return result
 
 
-def h(A, x, b, weight, offset=0):
+def h(A, x, b, weight, offset=0, mask=None):
     hess_list = []
     for A_i, x_i, b_i, w_i in zip(A, x, b, weight):
 
@@ -37,8 +39,12 @@ def h(A, x, b, weight, offset=0):
             t1 = x_i.T @ Q_k @ x_i - abs(b_i[k, :]) ** 2 - offset
             hess_k += 8 * t0 @ ((x_i.T @ Q_k) * x_i.T) \
                       + 4 * t1 * np.diag(x_i.squeeze()) @ Q_k @ np.diag(x_i.squeeze()) + 4 * t1 * np.diag(t0.squeeze())
-        hess_list.append(hess_k)
-    result = scipy.linalg.block_diag(*hess_list)
 
-    # TODO: add permutation based on mask of currents
+        hess_list.append(hess_k)
+
+    if mask is not None:
+        hess_list = [np.sum(np.array(hess_list), axis=0)]
+    result = scipy.linalg.block_diag(*hess_list)
     return result
+
+
