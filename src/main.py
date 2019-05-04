3166396c299b35f21ddaaee200540f6c6e7a4d82
Sergey Iakovlev <siakovlev@student.unimelb.edu.au>
2019-05-04 18:07:21 +1000

    SI: clean main.py

--- src/main.py
+++ src/main.py
@@ -12,19 +12,11 @@ def main(**kwargs):
     antenna.set_objective(weights=[1.0, 1.0])
     antenna.set_jacobian(weights=[1.0, 1.0])
     antenna.set_hessian(weights=[1.0, 1.0])
-    # antenna.get_optimal_current_allocation(opt_params, x0=-np.ones(antenna.N))
-    # q = np.array([-4.65348429, -4.65348429, -0.34661317, -0.34661317])
-    # q = np.array([-0.34661317, -0.34661317])
-    # print(antenna.objective(q))
-    # print(antenna.jac(q))
-    # print(antenna.hess(q))
-
+    antenna.get_optimal_current_allocation(opt_params, cons=False, x0=np.ones(antenna.N))
 
     # antenna.set_allocation_constraint(eps=7)
     # antenna.set_power_constraint(delta=0.5)
-
-    antenna.get_optimal_current_allocation(opt_params, cons=False, x0=np.ones(antenna.N))
-
+    
     antenna.plot_current_distribution()
     antenna.plot_formed_beams()
 
