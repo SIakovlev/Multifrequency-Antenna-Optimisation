d852960a18b786e6a7e2344ae23b5a8eab9650a6
Sergey Iakovlev <siakovlev@student.unimelb.edu.au>
2019-08-08 16:57:51 +1000

    SI: main clean up

--- src/main.py
+++ src/main.py
@@ -14,34 +14,24 @@ def main(**kwargs):
     antenna.set_objective(weights=[1.0, 1.0])
     antenna.set_jacobian(weights=[1.0, 1.0])
     antenna.set_hessian(weights=[1.0, 1.0])
+    antenna.plot_ref_beams()
     probe = None
-    for e in np.arange(0, 7, 0.01):
-        antenna.set_allocation_constraint(e)
-        _, probe = antenna.get_optimal_current_allocation(opt_params, x0=probe, cons=True, jac=True, hess=True)
-    # probe = np.random.rand(18)
-    # probe = np.array([0.52746065, 0.04358334, 0.30497155, 0.06462851, 0.23176169, 0.6587629,
-    #                   0.72621427, 0.98162255, 0.13840873, 0.58926654, 0.8000295, 0.14332016,
-    #                   0.87760155, 0.87915659, 0.68831623, 0.15874958, 0.04225982, 0.61493903])
-                      # 0.03477898, 0.8444315])
-    # print(f"Probe signal: \n{probe}\n")
+    # e = 2
+    # while e < 9:
+    #     print(f" - eps value: {e}")
+    #     antenna.set_allocation_constraint(e)
+    #     _, probe = antenna.get_optimal_current_allocation(opt_params, x0=probe, cons=True, jac=True, hess=True)
+    #     opt_params["options"] = {"maxiter": 5000, "verbose": 1, "initial_tr_radius": 1e6, "initial_barrier_parameter": 1e-6}
+    #     # antenna.plot_current_distribution()
+    #     # antenna.plot_formed_beams()
+    #     e = float(min(abs(antenna.M @ probe.reshape(-1, 1)))) - 0.1
+    #     print(f" - Probe: {probe}")
+    print(antenna.objective(probe))
+    print(np.linalg.matrix_rank(antenna.hess(probe), tol=1e-6))
 
     antenna.plot_current_distribution()
     antenna.plot_formed_beams()
 
-    # antenna2 = Antenna_new(antenna_params)
-    # antenna2.set_objective()
-    # antenna2.get_optimal_current_allocation(opt_params, x0=np.ones(200), cons=False, jac=False, hess=False)
-    #
-    # antenna2.plot_current_distribution()
-    # antenna2.plot_formed_beams()
-    # probe = np.ones(200)
-    # print(f"Probe signal: \n{probe}\n")
-    # print(f"Objective_f: \n{antenna2.objective(probe)}\n")
-    # print(f"Objective_grad_f: \n{antenna2.jac(probe)}\n")
-    # print(f"Objective_hess_f: \n{antenna2.hess(probe)}\n")
-
-    # print(f"Objective_f: \n{antenna.objective(probe)}\n")
-    # print(f"Objective_grad_f: \n{antenna.jac(probe)}\n")
 
 if __name__ == "__main__":
 
